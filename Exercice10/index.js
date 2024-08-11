9d3f186b3f2537e319ae79fff8c872b3165281ee
Fred jeune DEV en devenir <fredstessy@outlook.fr>
2024-08-11 14:27:17 +0200

    finish projet

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,14 @@
-//votre code ici
 
-export default printNumbers
+//votre code ici
+function printNumbers(n) {
+    let result = '';
+    for (let i = 1; i <= n; i++) {
+        result += i + ' ';
+    }
+    return result.trim(); // Supprime l'espace en trop à la fin
+}
+
+// Exemple d'utilisation :
+/* console.log(printNumbers(5));  Résultat : "1 2 3 4 5" */
+
+ export default printNumbers; 
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,16 @@
-// votre code ici
+function checkAge(age) {
+    if (age < 18) {
+        return "Vous êtes mineur.";
+    } else if (age >= 65) {
+        return "Vous êtes senior.";
+    } else {
+        return "Vous êtes majeur.";
+    }
+}
+
+console.log(checkAge(22)); // Affiche "Vous êtes majeur."
+console.log(checkAge(13)); // Affiche "Vous êtes mineur."
+console.log(checkAge(70)); // Affiche "Vous êtes senior."
+
 
 export default checkAge;
