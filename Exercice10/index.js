b149007106c6a40e69aabc04fe5f6ec89b3ecf12
Fred jeune DEV en devenir <fredstessy@outlook.fr>
2024-08-14 13:41:39 +0200

    ajout captures + exo 5 et 6 corriger

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -12,3 +12,14 @@ function printNumbers(n) {
 /* console.log(printNumbers(5));  Résultat : "1 2 3 4 5" */
 
  export default printNumbers; 
+
+/* 
+ autre facon de faire un peu plus complexe a comprendre 
+ function printNumbers(n) {
+    return Array.from({ length: n }, (_, i) => i + 1).join(' ');
+}
+
+// Exemple d'utilisation :
+/* console.log(printNumbers(5));  Résultat : "1 2 3 4 5" */
+
+/* export default printNumbers;  */
